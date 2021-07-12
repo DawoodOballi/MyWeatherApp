a55d0368e82dc2238fb3c48d7e7887dfccca5dd7
dawood oballi <d4w00d@hotmail.com>
2021-07-12 17:04:08 +0100

    Added in a button that takes me to second page with some FontAwesomeIcons

--- src/MyWeatherApp.js
+++ src/MyWeatherApp.js
@@ -1,20 +1,40 @@
 import React, {Component} from 'react';
 import './App.css';
+import logo from './logo.svg';
 import Button from 'react-bootstrap/Button';
 import {BrowserRouter as Router, Route, Switch, Link, Redirect} from 'react-router-dom';
 import MainPage from './Pages/index';
-import NotFoundPage from './Pages/404'
+import NotFoundPage from './Pages/404';
+
 
 class MyWeatherApp extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={MainPage}></Route>
-          <Route exact path="/404" component={NotFoundPage}></Route>
-          <Redirect to="/404/"/>
-        </Switch>
-      </Router>
+      <React.Fragment>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            <Router>
+              <Switch>
+                <Route exact path="/" component={MainPage}></Route>
+                <Route exact path="/404" component={NotFoundPage}></Route>
+                <Redirect to="/404/"/>
+              </Switch>
+            </Router>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      </React.Fragment>
     );
   }
 }
--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCloud} from '@fortawesome/free-solid-svg-icons'
 
 const MainPage = () => {
     return (
         <div>
             <h3>Welcome to react tutorial</h3>
             <small>Main Page</small>
+            <Button class="btn btn-danger">
+                <span>
+                    <Link to="/404">Cloud </Link>
+                    <FontAwesomeIcon icon={faCloud}/>
+                </span>
+            </Button>
         </div>
     );
 }
